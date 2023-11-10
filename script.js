8bb9e1776bf2af64ff8aff79f7cd36cbae5af0e6
Marko Arsic <markoarsyc@gmail.com>
2023-11-10 01:26:50 +0100

    All files uploaded

---
+++ script.js
@@ -0,0 +1,21 @@
+function pocetna() {
+	window.location.href = "index.html";
+}
+function polozaj() {
+	window.location.href = "polozaj.html";
+}
+function znamenitosti() {
+	window.location.href = "znamenitosti.html";
+}
+function istorija() {
+	window.location.href = "istorija.html";
+}
+function desavanja() {
+	window.location.href = "desavanja.html";
+}
+function nocni_zivot() {
+	window.location.href = "nocni_zivot.html";
+}
+function o_stranici() {
+	window.location.href = "o_stranici.html";
+}
